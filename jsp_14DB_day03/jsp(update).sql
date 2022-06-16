859267b6cafb65e6b22245a38f8adab6cb852a23
오혁진 <103741963+HyukJin1125@users.noreply.github.com>
2022-06-16 18:19:57 +0900

    Add files via upload

---
+++ jsp_14DB_day03/jsp(update).sql
@@ -0,0 +1,9 @@
+	 UPDATE                   	
+	        free_board    		
+	 SET		                
+		 bo_title = '제목'
+		,bo_category = 'BC01'		
+		,BO_CONTENT = '내용'		    
+		,bo_mod_date = sysdate	
+			WHERE bo_no= 2	
+            ;
